52e6beffb3f4e5c53765c62f7a72dffc03f022ca
Tristan Lacombe <tlacombevulca@outlook.fr>
2026-02-24 23:31:38 +0100

    feat(app): Implemented proper prompt for sending to model

--- src/hooks/usePrompt.tsx
+++ src/hooks/usePrompt.tsx
@@ -3,8 +3,23 @@ import type {
     ApiResponseOutput,
     OpenApiResponse,
 } from "../types/ModelResponse";
+import type { PromptReturn } from "../types/PromptReturn";
 
-export const usePrompt = () => {
+type UsePromptParam = {
+    model?: string;
+    setPromptMessage: (message: string) => void;
+};
+
+type UsePromptReturn = {
+    sendPrompt: (prompt: string) => Promise<PromptReturn | undefined>;
+    promptFormating: (promptMessage: string) => string;
+    loading: boolean;
+};
+
+export const usePrompt = ({
+    model = "gpt-5-mini",
+    setPromptMessage,
+}: UsePromptParam): UsePromptReturn => {
     const [loading, setLoading] = useState(false);
 
     const extractOutputText = (response: OpenApiResponse): string => {
@@ -21,7 +36,39 @@ export const usePrompt = () => {
         return message;
     };
 
-    const sendPrompt = async (prompt: string) => {
+    const promptFormating = (promptMessage: string) => {
+        const formattedPromt = `
+            Tu es un guide de voyage pour randonneurs. Mon utilisateur souhaite un itinéraire de randonnée suivant cette description:
+            "${promptMessage}", et tu dois lui répondre avec un itinéraire de randonnée détaillé, incluant les points d'intérêt, les distances, les temps de marche estimés, et les conseils pour la randonnée.
+            
+            Renvoie moi cet itinéraire sous la forme d'un JSON avec les champs suivants:
+            {
+                "destination": "Nom de la destination de la randonnée",
+                "depart": "Point de départ de la randonnée",
+                "arrivée": "Point d'arrivée de la randonnée, si c'est un boucle indique le même point que le départ",
+                "distance_totale": "Distance totale de la randonnée",
+                "temps_total_estime": "Temps total de marche estimé pour la randonnée",
+                "difficulté": "Niveau de difficulté de la randonnée (facile, modérée, difficile)",
+                "itineraire": [
+                    {
+                        "point_interet": "Nom du point d'intérêt",
+                        "coordonnees": "Coordonnées GPS du point d'intérêt",
+                        "description": "Description du point d'intérêt",
+                        "distance": "Distance depuis le point de départ ou le point précédent",
+                        "temps_estime": "Temps de marche estimé pour atteindre ce point depuis le point de départ ou le point précédent",
+                        "conseils": "Conseils spécifiques pour ce point d'intérêt, comme les meilleures périodes pour visiter, les équipements recommandés, etc."
+                    },
+                ],
+                "conseils_generaux": "Conseils généraux pour la randonnée, comme les précautions à prendre, les équipements à emporter",
+            }
+            `;
+
+        return formattedPromt;
+    };
+
+    const sendPrompt = async (
+        prompt: string,
+    ): Promise<PromptReturn | undefined> => {
         setLoading(true);
 
         try {
@@ -34,22 +81,25 @@ export const usePrompt = () => {
                         Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
                     },
                     body: JSON.stringify({
-                        model: "gpt-5-mini",
-                        input: prompt,
+                        model: model,
+                        input: promptFormating(prompt),
                     }),
                 },
             );
 
             const data = await response.json();
 
+            setPromptMessage("");
             setLoading(false);
 
-            return extractOutputText(data);
+            console.log("API Response:", JSON.parse(extractOutputText(data)));
+
+            return JSON.parse(extractOutputText(data));
         } catch (error) {
             setLoading(false);
             console.error("Error sending prompt:", error);
         }
     };
 
-    return { sendPrompt, loading };
+    return { sendPrompt, promptFormating, loading };
 };
---
+++ src/types/PromptReturn.ts
@@ -0,0 +1,19 @@
+export type ItinerairePoint = {
+    point_interet: string;
+    coordonnees: string;
+    description: string;
+    distance: string;
+    temps_estime: string;
+    conseils: string;
+};
+
+export type PromptReturn = {
+    destination: string;
+    depart: string;
+    arrivée: string;
+    trace_gpx?: string;
+    distance_totale: string;
+    temps_total_estime: string;
+    difficulté: "facile" | "modérée" | "difficile";
+    itineraire: ItinerairePoint[];
+};
